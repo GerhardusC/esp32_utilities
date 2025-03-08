01522cb87ebf3966bdb7d3bf17cbb6669b9bc683
Gerhardus <hardus.cronje@gmail.com>
2025-03-08 19:36:39 +0200

    fix: changed over to using new wiring

--- include/seven_seg.c
+++ include/seven_seg.c
@@ -5,17 +5,17 @@
  * Takes a normal number from 0-9 and converts it to the correct combination of pins to light up each number.*/
 uint8_t get_display_val_from_u8(uint8_t num) {
     switch(num){
-        case 0: return 0b00111111;
-        case 1: return 0b00110000;
-        case 2: return 0b01011011;
-        case 3: return 0b01111001;
-        case 4: return 0b01110100;
-        case 5: return 0b01101101;
-        case 6: return 0b01101111;
-        case 7: return 0b00111000;
-        case 8: return 0b01111111;
-        case 9: return 0b01111101;
-        default: return 0b10000000;
+        case 0: return 0b11101110;
+        case 1: return 0b10000010;
+        case 2: return 0b11001101;
+        case 3: return 0b01101101;
+        case 4: return 0b00101011;
+        case 5: return 0b01100111;
+        case 6: return 0b11100111;
+        case 7: return 0b00101100;
+        case 8: return 0b11101111;
+        case 9: return 0b01101111;
+        default: return 0b00010000;
     }
 }
 
--- include/shift_register.c
+++ include/shift_register.c
@@ -1,6 +1,6 @@
 #include "shift_register.h"
 
-#define SHIFT_REGISTER_VCC 22
+#define SHIFT_REGISTER_RESET 22
 #define SHIFT_REGISTER_A 23
 #define SHIFT_REGISTER_CLOCK 4
 
@@ -12,19 +12,16 @@ void toggle_shift_register_clock() {
 void write_to_shift_register(uint8_t val) {
     // Set data pin to val
     gpio_set_level(SHIFT_REGISTER_A, val);
-    vTaskDelay(5);
     // Tick clock
     toggle_shift_register_clock();
-    vTaskDelay(5);
     // reset data pin
     gpio_set_level(SHIFT_REGISTER_A, 0);
-    vTaskDelay(5);
 }
 
 void setup_shift_register() {
-    gpio_reset_pin(SHIFT_REGISTER_VCC);
-    gpio_set_direction(SHIFT_REGISTER_VCC, GPIO_MODE_OUTPUT);
-    gpio_set_level(SHIFT_REGISTER_VCC, 1);
+    gpio_reset_pin(SHIFT_REGISTER_RESET);
+    gpio_set_direction(SHIFT_REGISTER_RESET, GPIO_MODE_OUTPUT);
+    gpio_set_level(SHIFT_REGISTER_RESET, 1);
 
     gpio_reset_pin(SHIFT_REGISTER_A);
     gpio_set_direction(SHIFT_REGISTER_A, GPIO_MODE_OUTPUT);
