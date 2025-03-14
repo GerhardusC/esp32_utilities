5d8cf94b35fc6ac2c922f065321bcb042c5d022e
Gerhardus Cronje <hardus.cronje@gmail.com>
2025-03-14 22:36:25 +0200

    feat: added front end build stuff

--- api/src/main.rs
+++ api/src/main.rs
@@ -17,13 +17,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match listener {
         Ok(listener) => {
-            axum::serve(listener, app).await?;
             println!("Listening on port 80");
+            axum::serve(listener, app).await?;
         }
         Err(_) => {
             let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await?;
-            axum::serve(listener, app).await?;
             println!("Listening on port 8000");
+            axum::serve(listener, app).await?;
         }
     }
 
